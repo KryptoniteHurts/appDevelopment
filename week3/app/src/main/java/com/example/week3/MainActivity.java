baf245db3a18089da0140f2f0765d00b6693dc42
sfdsfsf <sfdsfsf>
2020-01-28 12:51:41 +0000

    week3

---
+++ week3/app/src/main/java/com/example/week3/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.week3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+    public void launchActivity(View v) {
+        Intent intent = new Intent(MainActivity.this, BrowserActivity.class);
+        intent.putExtra ("dataKey", "https://www.youtube.com");
+        startActivity(intent);
+    }
+}
